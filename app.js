4c631c47a14a31f0cdabf5ab96c28873c3b6ad27
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-06-19 00:37:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -178,6 +178,6 @@ app.post("/delete", function (req, res) {
 
 
 
-// app.listen(3000, function () {
-//   console.log("Server started on port 3000");
-// });
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
